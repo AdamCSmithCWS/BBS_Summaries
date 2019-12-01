bb47f97dfbb75abefca50af47f9d8ad5d573e082
Adam C Smith <adam.smith2@canada.ca>
2019-12-01 00:28:43 -0500

    COSEWIC plotting function

--- summary script.R
+++ summary script.R
@@ -12,12 +12,21 @@ p_load(char = c("bbsBayes","ggplot2","ggrepel","RColorBrewer"),character.only = 
 # 
 # species_to_run = unique(dat_strat$species_strat$english)
 
+c_orng = brewer.pal(9,"Set1")[5]
+c_red = brewer.pal(9,"Set1")[1]
+c_blue = brewer.pal(9,"Set1")[2]
+c_purp = brewer.pal(9,"Set1")[4]
+c_green = brewer.pal(9,"Set1")[3]
+
 
 speciestemp = c("Bobolink","McCown's Longspur","Canada Warbler","Western Wood-Pewee")
 
+rollTrend = "Trend"
 
 # qs = c(0.025,0.05,0.95,0.975)
 YYYY = 2018
+short_time = 10 #length of time for short-term trend
+
 
 for(ss in speciestemp){
  for(noise in c("heavy_tailed","normal_tailed")){ 
@@ -35,7 +44,7 @@ for(ss in speciestemp){
   #### loop for short-term and long-term indices and trends
   
   
-  for(fy in c(1970)){
+  for(fy in c(1970,YYYY-short_time)){
     plot_header = paste(ss,noise,fy,sep = "_")
     
   inds = generate_regional_indices(jags_mod = jags_mod,
@@ -124,10 +133,10 @@ for(ss in speciestemp){
     ttmin$lbl = "Observed means"
     
     np <- ttp + 
-      geom_point(data = ttind,aes(x = Year, y = obs_mean),colour = brewer.pal(5,"Set1")[2],alpha = 0.3)+
-      geom_col(data = ttind,aes(x = Year, y = prts.sc),width = 0.5,fill = brewer.pal(5,"Set1")[5],alpha = 0.1)+
-      geom_text_repel(data = ttmax,aes(x = Year, y = prts.sc,label = lbl),nudge_y = 0.1*ulim,colour = brewer.pal(5,"Set1")[5],alpha = 0.5,size = 3)+
-    geom_text_repel(data = ttmin,aes(x = Year, y = obs_mean,label = lbl),nudge_y = 0.1*ulim,colour = brewer.pal(5,"Set1")[2],alpha = 0.5,size = 3)+
+      geom_point(data = ttind,aes(x = Year, y = obs_mean),colour = c_blue,alpha = 0.3)+
+      geom_col(data = ttind,aes(x = Year, y = prts.sc),width = 0.5,fill = c_green,alpha = 0.1)+
+      geom_text_repel(data = ttmax,aes(x = Year, y = prts.sc,label = lbl),nudge_y = 0.1*ulim,colour = c_green,alpha = 0.5,size = 3)+
+    geom_text_repel(data = ttmin,aes(x = Year, y = obs_mean,label = lbl),nudge_y = 0.1*ulim,colour = c_blue,alpha = 0.5,size = 3)+
     annotate("text",x = mean(c(fy,YYYY)),y = ulim*0.9,label = trlab)
   print(np)
     
@@ -167,12 +176,13 @@ for(ss in speciestemp){
                                    #quantiles = qs,
                                    regions = c("continental","national"),
                                    startyear = fy2,
-                                   max_backcast = 5)
+                                   max_backcast = 5,
+                                   alternate_n = "n3")
 
   
-  for(ly2 in c((fy2+10):YYYY)){
+  for(ly2 in c((fy2+short_time):YYYY)){
   trst = generate_regional_trends(indices = indscos,
-                                 Min_year = ly2-10,
+                                 Min_year = ly2-short_time,
                                  Max_year = ly2,
                                  #quantiles = qs,
                                  slope = T,
@@ -186,15 +196,62 @@ for(ss in speciestemp){
   
   }
 
+  tcos$rolt = tcos[,rollTrend]
+  tcos$roltlci = tcos[,paste0(rollTrend,"_Q0.025")]
+  tcos$roltlci2 = tcos[,paste0(rollTrend,"_Q0.25")]
+  tcos$roltuci = tcos[,paste0(rollTrend,"_Q0.975")]
+  tcos$roltuci2 = tcos[,paste0(rollTrend,"_Q0.75")]
+  
+  thresh30 = (0.7^(1/short_time)-1)*100
+  thresh50 = (0.5^(1/short_time)-1)*100
+  
+  threshs = data.frame(thresh = c(thresh30,thresh50),
+                       p_thresh = c(paste("-30% over",short_time,"years"),
+                                          paste("-50% over",short_time,"years")),
+                       Year = rep(min(tcos$End_year),2))
+  
   pdf(paste0("output/Rolling_Trends/",plot_header,"_Rolling_Trends.pdf"),
       width = 8.5,
       height = 6)
   for(rg in unique(tcos$Region_alt)){
     
     tmp = tcos[which(tcos$Region_alt == rg),]
+    tmpend4 = tmp[nrow(tmp)-4,]
+    tmpend4$lab50 = "50% CI"
+    tmpend4$lab95 = "95% CI"
     
+    tmpend = tmp[nrow(tmp),]
     
-  }
+    pth_30_labs = paste0(signif(100*tmpend[,"prob_decrease_30_percent"],2),"% probability of 30% decrease") 
+    pth_50_labs = paste0(signif(100*tmpend[,"prob_decrease_50_percent"],2),"% probability of 50% decrease") 
+    tmpend$pdec = paste(signif(tmpend[,"Percent_Change"],2),"% Change over",short_time,"years") 
+    
+      
+    cpt = ggplot(data = tmp,aes(x = End_year,y = rolt))+
+      theme_minimal()+
+      labs(title = paste(ss,"rolling",short_time,"year trends",rg),
+           subtitle = paste("Based on",rollTrend,"in",YYYY,":",pth_30_labs,"and",pth_50_labs))+
+           xlab(paste("Ending year of",short_time,"trend"))+
+           ylab(paste(short_time,"year trends"))+
+      geom_hline(yintercept = thresh30,colour = c_orng)+
+      geom_hline(yintercept = thresh50,colour = c_red)+
+      geom_hline(yintercept = 0,colour = grey(0.5))+
+      geom_label_repel(data = threshs,aes(x = Year,y = thresh,label = p_thresh),position = "nudge")+
+      geom_linerange(aes(x = End_year,ymin = roltlci,ymax = roltuci),colour = c_blue,alpha = 0.6,size = 0.9)+
+      geom_pointrange(aes(x = End_year,y = rolt,ymin = roltlci2,ymax = roltuci2),colour = c_blue,size = 1.3,fatten = 1)+
+      geom_text_repel(data = tmpend,aes(x = End_year,y = rolt,label = pdec),min.segment.length = 0.1,nudge_y = -0.5)+
+      geom_text_repel(data = tmpend4,aes(x = End_year,y = roltuci,label = lab95),colour = c_blue,alpha = 0.6,min.segment.length = 0.1,nudge_y = 0.5,nudge_x = 0.5)+
+      geom_text_repel(data = tmpend4,aes(x = End_year,y = roltlci2,label = lab50),colour = c_blue,min.segment.length = 0.1,nudge_y = -0.5,nudge_x = -0.5)
+      
+      
+      
+
+    ## add final probability of decrease at thresholds
+    ## add species name and trend_type
+    ## update the theme?
+  print(cpt)
+    
+    }
   dev.off()
   ###############################################################
   ###############################################################
