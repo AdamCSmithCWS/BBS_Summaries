8663bc3e90683a7bf033723a2e141465fe82bc98
Adam C Smith <adam.smith2@canada.ca>
2020-02-27 18:03:54 -0500

    readme update

---
+++ Extracting regional data.R
@@ -0,0 +1,33 @@
+### script for extracting regional summaries of data
+
+### species-level data can be gleaned from the species-specific files
+### in the Google Drive folder:
+https://drive.google.com/open?id=1OTqdql4oGTGqwDNWcEIHtpamrSifvp-f
+
+### The summary files that include all of the species trends and annual
+### indices are (far) too large to be viewed in excel.
+
+### the best way to extract information for a region is through R
+### or some other data-management tool that can handle csv files with 
+### Millions of lines
+
+### if you'd like to extract everything for a particular region,
+
+
+
+### lumping and splitting notes:
+# For some species, the BBS dataset has split species complexes based on geography
+# When the underlying dataset has split the observations, I have followed those
+# splits. So, for these groups, combined estimates are no longer produced
+
+Sapsucker data are not longer combined to produce estimates
+
+
+
+
+
+
+
+
+
+
